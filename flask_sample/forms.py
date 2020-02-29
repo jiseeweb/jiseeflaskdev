6642f5b7b6a06f6d022229ed3a8ef5e6a99b5dcc
Jisee <jc.vinceedev@gmail.com>
2020-02-29 19:19:52 +0800

    Initial commit

---
+++ flask_sample/forms.py
@@ -0,0 +1,57 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from flask_sample.models import User
+from flask_login import current_user
+from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
+
+class RegistrationForm(FlaskForm):
+	username = StringField('Username', validators=[DataRequired(), Length(min=3, max=12)])
+	email = StringField('Email', validators=[DataRequired(), Email()])
+	password = PasswordField('Password', validators=[DataRequired()])
+	confirmPassword = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+	submit = SubmitField('Sign up')
+
+	# Custom Validation Autocalled?
+	def validate_username(self, username):
+		user = User.query.filter_by(username=username.data).first()
+		if user:
+			raise ValidationError('Username already exist. Please choose another one.')
+
+	def validate_email(self, email):
+		user = User.query.filter_by(email=email.data).first()
+		if user:
+			raise ValidationError('E-mail already exist. Please choose another one.')
+
+class LoginForm(FlaskForm):
+	email = StringField('Email', validators=[DataRequired(), Email()])
+	password = PasswordField('Password', validators=[DataRequired()])
+	remember = BooleanField('Remember Me')
+	submit = SubmitField('Login')
+
+
+class UpdateAccountForm(FlaskForm):
+	username = StringField('Username', validators=[DataRequired(), Length(min=4, max=12)])
+	email = StringField('Email', validators=[DataRequired(), Email()])
+	photo = FileField('Profile Photo', validators=[FileAllowed(['png', 'jpg'])])
+	submit = SubmitField('Update')
+
+	# Custom Validation Autocalled? YES
+	def validate_username(self, username):
+		if current_user.username != username.data:
+			user = User.query.filter_by(username=username.data).first()
+			if user:
+				raise ValidationError('Username already exist. Please choose another one.')
+
+	def validate_email(self, email):
+		if current_user.email != email.data:
+			user = User.query.filter_by(email=email.data).first()
+			if user:
+				raise ValidationError('E-mail already exist. Please choose another one.')
+
+
+class PostForm(FlaskForm):
+
+	title = StringField('Title', validators=[DataRequired()])
+	content = TextAreaField('Content', validators=[DataRequired()])
+	submit = SubmitField('Post')
---
+++ flask_sample/routes.py
@@ -0,0 +1,127 @@
+import secrets, os
+from PIL import Image
+from flask import render_template, flash, redirect, url_for, request
+from flask_sample import app, db, bcrypt
+from flask_sample.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
+from flask_sample.models import User, Post
+from flask_login import login_user, current_user, logout_user, login_required
+''' Take note this functions variables login_user, current_user, 
+logout_user are available outside of this module or in views. 
+It is accessible via template (eg; register.html)'''
+
+
+@app.route('/')
+@app.route('/home')
+def home_page():
+	posts = Post.query.all()
+	return render_template('home.html', title = 'Homepage', posts = posts )
+
+
+@app.route('/about')
+def about():
+	return render_template('about.html', title='About')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+
+	if current_user.is_authenticated:
+		return redirect(url_for('home_page'))
+
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+		user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+		db.session.add(user)
+		db.session.commit()
+		flash(f'Account has been created for {form.username.data}!', category='success') # One time alert
+		return redirect(url_for('home_page'))
+
+	return render_template('register.html', title='Register', form = form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+	#Check if the user is logged in
+	if current_user.is_authenticated:
+		return redirect(url_for('home_page'))
+
+	form = LoginForm()
+	# Authenticate User
+	if form.validate_on_submit():
+		user = User.query.filter_by(email=form.email.data).first()
+		if user and bcrypt.check_password_hash(user.password, form.password.data):
+			login_user(user, remember=form.remember.data)
+			next_page = request.args.get('next') # Access query parameter 
+			return redirect(next_page) if next_page else redirect(url_for('home_page')) # Ternary Conditional
+		else:
+			flash('Login Unsuccessful. Please check email or password.', category='danger')
+
+	return render_template('login.html', title='Login', form = form)
+
+
+@app.route('/logout')
+def logout():
+	logout_user()
+	return redirect(url_for('login'))
+
+
+def photo_save(form_photo): # Use to resize and rename profile pictures.
+	random = secrets.token_hex(8)
+	_, fext = os.path.splitext(form_photo.filename)
+	fname = random + fext
+	photopath = os.path.join(app.root_path, 'static/profile', fname)
+
+	output_size = 125, 125
+
+	img = Image.open(form_photo)
+	img.thumbnail(output_size)
+	img.save(photopath)
+
+	return fname
+
+
+
+@app.route('/account', methods=['GET', 'POST'])
+@login_required # Restricted route which requires users to login.
+def account():
+	form = UpdateAccountForm()
+
+	if form.validate_on_submit():
+		#check_user = User.query.filter_by(username=form.username.data).first()
+		if form.photo.data:
+			user_photo = photo_save(form.photo.data)
+			current_user.image_file = user_photo
+		current_user.username = form.username.data
+		current_user.email = form.email.data
+		db.session.commit()
+		flash('Your profile has been successfully updated!', 'success')
+		return redirect(url_for('account'))
+
+	elif request.method == 'GET':
+		form.username.data = current_user.username
+		form.email.data = current_user.email
+
+	image_file = url_for('static', filename='profile/' + current_user.image_file)
+	return render_template('account.html', title='Account', image_file=image_file, form = form)
+
+
+@app.route('/post/new', methods=['GET', 'POST'])
+@login_required # Restricted route which requires users to login.
+def new_post():
+	form = PostForm()
+	if form.validate_on_submit():
+		post = Post(title=form.title.data, content=form.content.data, author = current_user)
+		db.session.add(post)
+		db.session.commit()
+		flash('Your post has been successfully created!', 'success')
+		return redirect(url_for('home_page'))
+	return render_template('create_post.html', title='Create Post', form = form)
+
+
+@app.route('/post/<int:post_id>')
+@login_required # Restricted route which requires users to login.
+def post(post_id):
+	post = Post.query.get_or_404(post_id)
+	return render_template('post.html', title=post.title, post = post)
